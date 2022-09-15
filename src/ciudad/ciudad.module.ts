77447d4644a10bab3ee89a941f645390f6d52832
Veiko Quiroz <v.quiroz@uniandes.edu.co>
2022-09-15 11:59:20 -0500

    Se agrega el Punto 2 - lógica de ciudad

--- src/ciudad/ciudad.module.ts
+++ src/ciudad/ciudad.module.ts
@@ -1,4 +1,11 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CiudadEntity } from './ciudad.entity';
+import { CiudadService } from './ciudad.service';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([CiudadEntity])],
+  providers: [CiudadService]
+})
 export class CiudadModule {}
---
+++ src/ciudad/ciudad.service.ts
@@ -0,0 +1,59 @@
+/* eslint-disable prettier/prettier */
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { BusinessLogicException, BusinessError } from '../shared/errors/business-errors';
+import { Repository } from 'typeorm';
+import { CiudadEntity } from './ciudad.entity';
+
+@Injectable()
+export class CiudadService {
+    constructor(
+        @InjectRepository(CiudadEntity)
+        private readonly ciudadRepository: Repository<CiudadEntity>
+    ){}
+
+    async findAll(): Promise<CiudadEntity[]> {
+        return await this.ciudadRepository.find({ relations: ["supermercados"] });
+    }
+
+    async findOne(id: string): Promise<CiudadEntity> {
+        const ciudad: CiudadEntity = await this.ciudadRepository.findOne({where: {id}, relations: ["supermercados"] } );
+        if (!ciudad)
+          throw new BusinessLogicException("No se encontró la ciudad con el ID dado", BusinessError.NOT_FOUND);
+   
+        return ciudad;
+    }
+
+    async create(ciudad: CiudadEntity): Promise<CiudadEntity> {            
+        if( !ValidaPais(ciudad.pais))
+            throw new BusinessLogicException("El pais de la ciudad ingresada no se encuentra dentro de los paises permitidos", BusinessError.NOT_FOUND);  
+        return await this.ciudadRepository.save(ciudad);
+    }
+
+    async update(id: string, ciudad: CiudadEntity): Promise<CiudadEntity> {
+        if( !ValidaPais(ciudad.pais))
+            throw new BusinessLogicException("El pais de la ciudad ingresada no se encuentra dentro de los paises permitidos", BusinessError.NOT_FOUND);  
+
+        const persistedCiudad: CiudadEntity = await this.ciudadRepository.findOne({where:{id}});
+        if (!persistedCiudad)
+          throw new BusinessLogicException("No se encontró la ciudad con el ID dado", BusinessError.NOT_FOUND);
+        
+        return await this.ciudadRepository.save({...persistedCiudad, ...ciudad});
+    }
+
+    async delete(id: string) {
+        const ciudad: CiudadEntity = await this.ciudadRepository.findOne({where:{id}});
+        if (!ciudad)
+          throw new BusinessLogicException("No se encontró la ciudad con el ID dado", BusinessError.NOT_FOUND);
+      
+        await this.ciudadRepository.remove(ciudad);
+    }
+
+
+}
+
+function ValidaPais(pais: string) {
+    const listaDePaises = ["ARGENTINA", "ECUADOR", "PARAGUAY"];
+    return listaDePaises.includes(pais.toUpperCase());
+}
+
