b3333a544dd90927f51da858851a3fd67297fe57
Veiko Quiroz <v.quiroz@uniandes.edu.co>
2022-09-15 23:16:50 -0500

    Se agrega el Punto 3 - Controlador para supermercado

--- src/supermercado/supermercado.module.ts
+++ src/supermercado/supermercado.module.ts
@@ -3,9 +3,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { SupermercadoEntity } from './supermercado.entity';
 import { SupermercadoService } from './supermercado.service';
+import { SupermercadoController } from './supermercado.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([SupermercadoEntity])],
-  providers: [SupermercadoService]
+  providers: [SupermercadoService],
+  controllers: [SupermercadoController]
 })
 export class SupermercadoModule {}
