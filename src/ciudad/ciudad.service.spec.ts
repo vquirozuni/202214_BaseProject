f343eef165450383a45ca07107fa2ca881e00970
Veiko Quiroz <v.quiroz@uniandes.edu.co>
2022-09-15 19:28:33 -0500

    Se agrega el Punto 2 - pruebas para la lógica Ciudad y Supermercado

--- src/ciudad/ciudad.service.spec.ts
+++ src/ciudad/ciudad.service.spec.ts
@@ -1,18 +1,136 @@
+/* eslint-disable prettier/prettier */
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
 import { CiudadService } from './ciudad.service';
+import { Repository } from 'typeorm';
+import { CiudadEntity } from './ciudad.entity';
+import { TypeOrmTestingConfig } from '../shared/testing-utils/typeorm-testing-config';
+import { faker } from '@faker-js/faker';
 
 describe('CiudadService', () => {
   let service: CiudadService;
+  let repository: Repository<CiudadEntity>;
+  let ciudadList: CiudadEntity[];
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [...TypeOrmTestingConfig()],
       providers: [CiudadService],
     }).compile();
 
     service = module.get<CiudadService>(CiudadService);
+    repository = module.get<Repository<CiudadEntity>>(getRepositoryToken(CiudadEntity));
+    await seedDatabase();
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  const seedDatabase = async () => {
+    repository.clear();
+    ciudadList = [];
+    for(let i = 0; i < 5; i++){
+        const ciudad: CiudadEntity = await repository.save(
+          {
+            nombre: faker.address.cityName(),
+            pais: faker.address.country(),
+            numeroDeHabitantes: parseInt(faker.random.numeric(6))
+          }
+        );        
+        ciudadList.push(ciudad);
+    }
+  }
+
+  it('findAll debería devolver todas las ciudades', async () => {
+    const ciudades: CiudadEntity[] = await service.findAll();
+    expect(ciudades).not.toBeNull();
+    expect(ciudades).toHaveLength(ciudadList.length);
   });
+
+  it('findOne debería retornar una ciudad por id', async () => {
+    const storedCiudad: CiudadEntity = ciudadList[0];
+    const ciudad: CiudadEntity = await service.findOne(storedCiudad.id);
+    expect(ciudad).not.toBeNull();
+    expect(ciudad.nombre).toEqual(storedCiudad.nombre)
+    expect(ciudad.pais).toEqual(storedCiudad.pais)
+    expect(ciudad.numeroDeHabitantes).toEqual(storedCiudad.numeroDeHabitantes)
+  });
+
+  it('findOne debería lanzar una excepción para una ciudad inválida', async () => {
+    await expect(() => service.findOne("0")).rejects.toHaveProperty("message", "No se encontró la ciudad con el ID dado")
+  });
+
+
+  it('create debería retornar una nueva ciudad de un pais permitido', async () => {
+    const ciudad: CiudadEntity = {
+      id: "",
+      nombre: faker.address.cityName(),
+      pais: "Argentina",
+      numeroDeHabitantes: parseInt(faker.random.numeric(6)),
+      supermercados: []
+    }
+ 
+    const nuevaCiudad: CiudadEntity = await service.create(ciudad);
+    expect(nuevaCiudad).not.toBeNull();
+ 
+    const storedCiudad: CiudadEntity = await repository.findOne({where: {id: nuevaCiudad.id}})
+    expect(storedCiudad).not.toBeNull();
+    expect(storedCiudad.nombre).toEqual(nuevaCiudad.nombre)
+    expect(storedCiudad.pais).toEqual(nuevaCiudad.pais)
+    expect(storedCiudad.numeroDeHabitantes).toEqual(nuevaCiudad.numeroDeHabitantes)
+  });  
+
+  it('create debería lanzar una excepción por crear una nueva ciudad de un pais no permitido', async () => {
+    const ciudad: CiudadEntity = {
+      id: "",
+      nombre: faker.address.cityName(),
+      pais: faker.address.country(),
+      numeroDeHabitantes: parseInt(faker.random.numeric(6)),
+      supermercados: []
+    } 
+    await expect(() => service.create(ciudad)).rejects.toHaveProperty("message", "El pais de la ciudad ingresada no se encuentra dentro de los paises permitidos")
+  });  
+
+  
+  it('update debería modificar una ciudad con datos de un pais permitido', async () => {
+    const ciudad: CiudadEntity = ciudadList[0];
+    ciudad.nombre = faker.address.cityName();
+    ciudad.pais = "Paraguay";
+    ciudad.numeroDeHabitantes = parseInt(faker.random.numeric(6));
+    const updatedCiudad: CiudadEntity = await service.update(ciudad.id, ciudad);
+    expect(updatedCiudad).not.toBeNull();
+     const storedCiudad: CiudadEntity = await repository.findOne({ where: { id: ciudad.id } })
+    expect(storedCiudad).not.toBeNull();
+    expect(storedCiudad.nombre).toEqual(ciudad.nombre)
+    expect(storedCiudad.pais).toEqual(ciudad.pais)
+    expect(storedCiudad.numeroDeHabitantes).toEqual(ciudad.numeroDeHabitantes)
+  });
+
+
+  it('update debería lanzar una excepción por modificar una ciudad con datos de un pais no permitido', async () => {
+    const ciudad: CiudadEntity = ciudadList[0];
+    ciudad.nombre = faker.address.cityName();
+    ciudad.pais = faker.address.country();
+    ciudad.numeroDeHabitantes = parseInt(faker.random.numeric(6));    
+    await expect(() => service.update(ciudad.id, ciudad)).rejects.toHaveProperty("message", "El pais de la ciudad ingresada no se encuentra dentro de los paises permitidos")
+  });
+
+  it('update debería lanzar una excepción por actualizar una ciudad que no existe', async () => {
+    let ciudad: CiudadEntity = ciudadList[0];
+    ciudad = {
+      ...ciudad, nombre: faker.address.cityName(), pais: "Argentina", numeroDeHabitantes: parseInt(faker.random.numeric(6))
+    }
+    await expect(() => service.update("0", ciudad)).rejects.toHaveProperty("message", "No se encontró la ciudad con el ID dado")
+  });
+
+  it('delete deberia eliminar una ciudad', async () => {
+    const ciudad: CiudadEntity = ciudadList[0];
+    await service.delete(ciudad.id);
+    const deletedCiudad: CiudadEntity = await repository.findOne({ where: { id: ciudad.id } })
+    expect(deletedCiudad).toBeNull();
+  });
+
+  it('delete debería lanzar una excepción por eliminar una ciudad que no existe', async () => {
+    const ciudad: CiudadEntity = ciudadList[0];
+    await service.delete(ciudad.id);
+    await expect(() => service.delete("0")).rejects.toHaveProperty("message", "No se encontró la ciudad con el ID dado")
+  });
+
 });
--- src/supermercado/supermercado.service.spec.ts
+++ src/supermercado/supermercado.service.spec.ts
@@ -1,18 +1,146 @@
+/* eslint-disable prettier/prettier */
 import { Test, TestingModule } from '@nestjs/testing';
 import { SupermercadoService } from './supermercado.service';
+import { SupermercadoEntity } from './supermercado.entity';
+import { Repository } from 'typeorm';
+import { TypeOrmTestingConfig } from '../shared/testing-utils/typeorm-testing-config';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { faker } from '@faker-js/faker';
+
 
 describe('SupermercadoService', () => {
   let service: SupermercadoService;
+  let repository: Repository<SupermercadoEntity>;
+  let supermercadoList: SupermercadoEntity[];
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [...TypeOrmTestingConfig()],
       providers: [SupermercadoService],
     }).compile();
 
     service = module.get<SupermercadoService>(SupermercadoService);
+    repository = module.get<Repository<SupermercadoEntity>>(getRepositoryToken(SupermercadoEntity));
+    await seedDatabase();
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  const seedDatabase = async () => {
+    repository.clear();
+    supermercadoList = [];
+    for(let i = 0; i < 5; i++){
+        const supermercado: SupermercadoEntity = await repository.save(
+          {
+            nombre: faker.company.name(),
+            longitud: faker.address.longitude(),
+            latitud: faker.address.latitude(),
+            paginaWeb: faker.internet.url()
+          }
+        );        
+        supermercadoList.push(supermercado);
+    }
+  }
+
+  it('findAll debería devolver todas los supermercados', async () => {
+    const supermercados: SupermercadoEntity[] = await service.findAll();
+    expect(supermercados).not.toBeNull();
+    expect(supermercados).toHaveLength(supermercadoList.length);
   });
+
+  it('findOne debería retornar un supermercado por id', async () => {
+    const storedSupermercado: SupermercadoEntity = supermercadoList[0];
+    const supermercado: SupermercadoEntity = await service.findOne(storedSupermercado.id);
+    expect(supermercado).not.toBeNull();
+    expect(supermercado.nombre).toEqual(storedSupermercado.nombre)
+    expect(supermercado.longitud).toEqual(storedSupermercado.longitud)
+    expect(supermercado.latitud).toEqual(storedSupermercado.latitud)
+    expect(supermercado.paginaWeb).toEqual(storedSupermercado.paginaWeb)
+  });
+
+  it('findOne debería lanzar una excepción para una supermercado inválido', async () => {
+    await expect(() => service.findOne("0")).rejects.toHaveProperty("message", "No se encontró el supermercado con el ID dado")
+  });
+
+
+  it('create debería retornar un nuevo supermercado con nombre válido', async () => {
+    const supermercado: SupermercadoEntity = {
+      id: "",
+      nombre: "Supermercado Nuevo",
+      longitud: faker.address.longitude(),
+      latitud: faker.address.latitude(),
+      paginaWeb: faker.internet.url(),
+      ciudades: []
+    }
+ 
+    const nuevoSupermercado: SupermercadoEntity = await service.create(supermercado);
+    expect(nuevoSupermercado).not.toBeNull();
+ 
+    const storedSupermercado: SupermercadoEntity = await repository.findOne({where: {id: nuevoSupermercado.id}})
+    expect(storedSupermercado).not.toBeNull();
+    expect(storedSupermercado.nombre).toEqual(nuevoSupermercado.nombre)
+    expect(storedSupermercado.longitud).toEqual(nuevoSupermercado.longitud)
+    expect(storedSupermercado.latitud).toEqual(nuevoSupermercado.latitud)
+    expect(storedSupermercado.paginaWeb).toEqual(nuevoSupermercado.paginaWeb)
+  });  
+
+  it('create debería lanzar una excepción por crear un nuevo supermercado con nombre no válido', async () => {
+    const supermercado: SupermercadoEntity = {
+      id: "",
+      nombre: "Nuevo",
+      longitud: faker.address.longitude(),
+      latitud: faker.address.latitude(),
+      paginaWeb: faker.internet.url(),
+      ciudades: []
+    } 
+    await expect(() => service.create(supermercado)).rejects.toHaveProperty("message", "El nombre del supermercado debe tener mas de 10 caracteres")
+  });  
+
+  
+  it('update debería modificar un supermercado con nombre válido', async () => {
+    const supermercado: SupermercadoEntity = supermercadoList[0];
+    supermercado.nombre = "Nuevo Nombre Supermercado";
+    supermercado.longitud = faker.address.longitude();
+    supermercado.latitud = faker.address.latitude();
+    supermercado.paginaWeb = faker.internet.url();
+    const updatedSupermercado: SupermercadoEntity = await service.update(supermercado.id, supermercado);
+    expect(updatedSupermercado).not.toBeNull();
+    const storedSupermercado: SupermercadoEntity = await repository.findOne({ where: { id: supermercado.id } })
+    expect(storedSupermercado).not.toBeNull();
+    expect(storedSupermercado.nombre).toEqual(supermercado.nombre)
+    expect(storedSupermercado.longitud).toEqual(supermercado.longitud)
+    expect(storedSupermercado.latitud).toEqual(supermercado.latitud)
+    expect(storedSupermercado.paginaWeb).toEqual(supermercado.paginaWeb)
+  });
+
+
+  it('update debería lanzar una excepción por modificar un supermercado con nombre no válido', async () => {
+    const supermercado: SupermercadoEntity = supermercadoList[0];
+    supermercado.nombre = "Nuevo";
+    supermercado.longitud = faker.address.longitude();
+    supermercado.latitud = faker.address.latitude();
+    supermercado.paginaWeb = faker.internet.url();
+    await expect(() => service.update(supermercado.id, supermercado)).rejects.toHaveProperty("message", "El nombre del supermercado debe tener mas de 10 caracteres")
+  });
+
+  it('update debería lanzar una excepción por actualizar un supermercado que no existe', async () => {
+    let supermercado: SupermercadoEntity = supermercadoList[0];
+    supermercado = {
+      ...supermercado, nombre: "Nuevo Nombre Supermercado", longitud: faker.address.longitude(), latitud: faker.address.latitude(), paginaWeb: faker.internet.url()
+    }
+    await expect(() => service.update("0", supermercado)).rejects.toHaveProperty("message", "No se encontró el supermercado con el ID dado")
+  });
+
+  it('delete deberia eliminar una supermercado', async () => {
+    const supermercado: SupermercadoEntity = supermercadoList[0];
+    await service.delete(supermercado.id);
+    const deletedSupermercado: SupermercadoEntity = await repository.findOne({ where: { id: supermercado.id } })
+    expect(deletedSupermercado).toBeNull();
+  });
+
+  it('delete debería lanzar una excepción por eliminar un supermercado que no existe', async () => {
+    const supermercado: SupermercadoEntity = supermercadoList[0];
+    await service.delete(supermercado.id);
+    await expect(() => service.delete("0")).rejects.toHaveProperty("message", "No se encontró el supermercado con el ID dado")
+  });
+
+  
 });
